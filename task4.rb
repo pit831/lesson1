d337bf04ab3d4d9d1bb28d69ced3737035020e44
pit831 <pit831@yandex.ru>
2019-09-26 17:19:40 +0300

    add backspaces

--- task4.rb
+++ task4.rb
@@ -9,11 +9,11 @@ d = b ** 2 - 4 * a * c
 if d < 0
   puts "#{d} - отрицательный D. Корней нет"
 elsif d == 0
-  x1 = -b/2*a
+  x1 = - b / 2 * a
   puts "Ответ: D=0, x=#{x1}"
 elsif d > 0
   sqrt_d = Math.sqrt(d)
-  x1 = (-b+sqrt_d)/2*a
-  x2 = (-b-sqrt_d)/2*a
+  x1 = (- b + sqrt_d) / 2 * a
+  x2 = (- b - sqrt_d) / 2 * a
   puts "Ответ: D=#{d}, x1=#{x1}, x2=#{x2}"
 end
